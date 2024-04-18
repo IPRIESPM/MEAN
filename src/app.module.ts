d9f296785cb0e4665b4cd08a2eeb477f5da3e931
Isaac <105789412+altaskur@users.noreply.github.com>
2024-04-18 13:01:24 +0200

    feat: :sparkles: add .env for conect db

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017'), AuthModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URI),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
